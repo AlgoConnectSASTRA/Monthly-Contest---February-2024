3db3296e3780137aa9fc0152d87f874cfa6c8f54
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-02-25 20:00:43 +0530

    Update Solution.py

--- Colosseum Leaderboard/Solution.py
+++ Colosseum Leaderboard/Solution.py
@@ -4,3 +4,13 @@
 # find the rank of SASTRA in the Leaderboard by counting the number of victories for each team
 
 
+
+def solution(n, winners):
+    # Write your code here
+    dictt = defaultdict(int)
+    for i in winners:
+        dictt[i] += 1
+    leaderboard = sorted(dictt.values(),reverse= True)
+    for i in range(0,len(leaderboard)):
+        if leaderboard[i] == dictt[1]:
+            return i + 1
