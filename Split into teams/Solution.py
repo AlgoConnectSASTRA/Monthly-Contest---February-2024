2ce8753df4b5d680262eeb5b964bc7959dc42bd7
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-02-25 20:09:10 +0530

    Update Solution.py

--- Split into teams/Solution.py
+++ Split into teams/Solution.py
@@ -2,3 +2,9 @@
 # their scores are given in an array -> ith score belongs to the ith student
 # split the students into teams in a way that sum of scores of each team is equal
 # find the maximum number of teams that can be formed this way
+def solution(n):
+    # Write your code here
+    if n%2 == 0:
+        return n//2
+    else:
+        return math.ceil(n/2)
