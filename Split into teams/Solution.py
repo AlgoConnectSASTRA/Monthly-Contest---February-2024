42353092cf42210ea0de551441e8e9e5e71f7af9
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-02-25 18:01:28 +0530

    Create Solution.py

---
+++ Split into teams/Solution.py
@@ -0,0 +1,4 @@
+# students numbered from 1 to n
+# their scores are given in an array -> ith score belongs to the ith student
+# split the students into teams in a way that sum of scores of each team is equal
+# find the maximum number of teams that can be formed this way
