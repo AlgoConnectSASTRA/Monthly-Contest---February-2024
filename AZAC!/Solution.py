39a069a618978198a3a434a6115c38ad58afa43d
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-02-25 19:51:30 +0530

    Update Solution.py

--- AZAC!/Solution.py
+++ AZAC!/Solution.py
@@ -1,1 +1,15 @@
-#given a string s containing only lowercase alphabets, check if the subsequence 'azac' is present
+#given a string s containing only lowercase alphabets of length n, check if the subsequence 'azac' is present
+
+def solution(n, s):
+    # Write your code here
+    required = 'azac'
+    j = 0
+    i = 0
+    while j<4 and i<n:
+        if s[i]==required[j]:
+            j += 1
+        i += 1
+    if j==4:
+        return "YES"
+    else :
+        return "NO"
