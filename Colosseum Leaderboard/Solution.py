e8234c73e04c29a6e7fca50fc8b21d4a5a3b3f57
AlgoConnectSASTRA <161212994+AlgoConnectSASTRA@users.noreply.github.com>
2024-02-25 18:22:49 +0530

    Create Solution.py

---
+++ Colosseum Leaderboard/Solution.py
@@ -0,0 +1,6 @@
+# n -> total number of events
+# winners array -> ith element denotes winner of ith event
+# teams are represented with numbers and the SASTRA is represented as 1 always
+# find the rank of SASTRA in the Leaderboard by counting the number of victories for each team
+
+
